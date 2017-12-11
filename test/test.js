f55db8ab0a57692632c1e58e9951b786f7e9c66e
Stephen Roman <stephenjroman@protonmail.com>
2017-12-11 02:21:26 -0500

    Rename cb.js to Calcs & make funcs more accessable
    - also added mayerIndex and currentPrice fields to Calcs

--- test/test.js
+++ test/test.js
@@ -1,28 +1,28 @@
 let assert = require('assert');
 let moment = require('moment');
-let app = require('../src/libs/cb.js');
+let Calcs = require('../src/libs/Calcs.js');
 
-describe('synchronous', () => {
-    describe('app.makeSpotUrl(currencyPair, date)', () => {
+describe('synchronous functions', () => {
+    describe('Calcs.prototype.makeSpotUrl(currencyPair, date)', () => {
         it('should return a URL of format: ' +
             'https://api.coinbase.com/v2/prices/:currencypair/spot?date=:date', () => {
             assert.equal('https://api.coinbase.com/v2/prices/BTC-USD/spot?date=2016-02-15',
-                app.makeSpotUrl('BTC-USD', '2016-02-15'));
+                Calcs.prototype.makeSpotUrl('BTC-USD', '2016-02-15'));
         });
     });
-//
-    describe('app.getAvgPrice(days)', () => {
+
+    describe('Calcs.prototype.getAvgPrice(days)', () => {
         it('should return a real of the average price', () => {
-            assert.equal(683.1885714285714, app.getAvgPrice(
+            assert.equal(683.1885714285714, Calcs.prototype.getAvgPrice(
                 ['514.35', '625.23', '326.93', '853.85', '1059.93', '957.20', '444.83']
             ));
         })
     });
-//
-    describe('app.findDaysBetweenDates', () => {
+
+    describe('Calcs.prototype.findDaysBetweenDates', () => {
         it('should return the number of days in the range (inclusive)', () => {
             assert.equal(4,
-                app.findDaysBetweenDates(
+                Calcs.prototype.findDaysBetweenDates(
                     moment(new Date()).add(-3, 'days').toISOString().slice(0,10),
                     moment(new Date()).add(-7, 'days').toISOString().slice(0,10)
                 )
@@ -32,10 +32,10 @@ describe('synchronous', () => {
 });
 
 // ASYNC //
-describe('asynchronous', () => {
-    describe('app.getJSONFromApi(url, callback)', () => {
+describe('asynchronous functions', () => {
+    describe('Calcs.prototype.getJSONFromApi(url, callback)', () => {
         it('should retrieve the the price of any currency pair at any time', (done) => {
-            app.getJSONFromApi('https://api.coinbase.com/v2/prices/BTC-USD/spot', (json) => {
+            Calcs.prototype.getJSONFromApi('https://api.coinbase.com/v2/prices/BTC-USD/spot', (json) => {
                 assert.strictEqual(typeof json, typeof {});
                 console.log(json);
                 done();
@@ -43,9 +43,9 @@ describe('asynchronous', () => {
         })
     });
 
-    describe('app.getSpotPrice(currencyPair, date, callback)', () => {
+    describe('Calcs.prototype.getSpotPrice(currencyPair, date, callback)', () => {
         it('should retrieve the historical price at a certain date ', (done) => {
-            app.getSpotPrice('BTC-USD', '2016-01-01', (amt) => {
+            Calcs.prototype.getSpotPrice('BTC-USD', '2016-01-01', (amt) => {
                 assert.strictEqual(amt, '433.28');
                 console.log(amt);
                 done();
@@ -53,9 +53,9 @@ describe('asynchronous', () => {
         })
     });
 
-    describe('app.getCurrentPrice', () => {
+    describe('Calcs.prototype.getCurrentPrice', () => {
         it('should retrieve the current price', (done) => {
-            app.getCurrentPrice('BTC-USD', (amt) => {
+            Calcs.prototype.getCurrentPrice('BTC-USD', (amt) => {
                 assert.strictEqual(typeof amt, typeof '0.00');
                 console.log(amt);
                 done();
@@ -63,9 +63,9 @@ describe('asynchronous', () => {
         })
     });
 
-    describe('app.getDailyPricesInRange(currencyPair, startDate, endDate, callback)', () => {
+    describe('Calcs.prototype.getDailyPricesInRange(currencyPair, startDate, endDate, callback)', () => {
         it('should get a list of prices', (done) => {
-            app.getDailyPricesInRange(
+            Calcs.prototype.getDailyPricesInRange(
                 'BTC-USD',
                 moment(new Date()).add(-500, 'days').toISOString().slice(0, 10),
                 moment(new Date()).add(-498, 'days').toISOString().slice(0, 10),
@@ -77,7 +77,7 @@ describe('asynchronous', () => {
         });
 
         it('should get a list of prices', (done) => {
-            app.getDailyPricesInRange(
+            Calcs.prototype.getDailyPricesInRange(
                 'BTC-USD',
                 moment(new Date()).add(-5, 'days').toISOString().slice(0, 10),
                 moment(new Date()).add(-1, 'days').toISOString().slice(0, 10),
@@ -89,7 +89,7 @@ describe('asynchronous', () => {
         });
 
         it('should get a list of prices', (done) => {
-            app.getDailyPricesInRange(
+            Calcs.prototype.getDailyPricesInRange(
                 'BTC-USD',
                 moment(new Date()).add(-4, 'days').toISOString().slice(0, 10),
                 moment(new Date()).add(0, 'days').toISOString().slice(0, 10),
@@ -101,9 +101,9 @@ describe('asynchronous', () => {
         });
     });
 
-    describe('app.get200DayMovingAverage(currencyPair, callback)', () => {
+    describe('Calcs.prototype.get200DayMovingAverage(currencyPair, callback)', () => {
         it('should get prices and calculate an average', (done) => {
-            app.get200DayMovingAverage('BTC-USD', (amt) => {
+            Calcs.prototype.get200DayMovingAverage('BTC-USD', (amt) => {
                 assert.strictEqual(typeof amt, typeof 0.00);
                 console.log(amt);
                 done();
@@ -111,9 +111,9 @@ describe('asynchronous', () => {
         })
     });
 
-    describe('app.getMayerIndex(callback)', () => {
+    describe('Calcs.prototype.getMayerIndex(callback)', () => {
         it('should get the Mayer index', (done) => {
-            app.getMayerIndex( (index) => {
+            Calcs.prototype.getMayerIndex( (index) => {
                 assert.strictEqual(typeof index, typeof 0.00);
                 console.log(index);
                 done();
