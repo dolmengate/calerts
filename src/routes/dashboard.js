58e0c6fd75a8da3f3e6ed07e35d3c33d5eaa91a3
Stephen Roman <stephenjroman@protonmail.com>
2017-12-10 19:08:05 -0500

    Move HTTP-specific app settings into app.js

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,21 +1,9 @@
+
 const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-
-const app = express();
-const PORT = '8080';
-
-const options = { extensions: ['js'] };
-express.static(path.join(__dirname, 'public'), options);
-
-app.set('views', path.join(__dirname, '../views'));
-app.set('view engine', 'pug');
-
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.json());
+const router = express.Router();
 
 // main route
-app.route('/cb-alerts/dashboard')
+router.route('/')
     .get((req, res) => {
         res.render('dashboard');
     })
@@ -26,12 +14,4 @@ app.route('/cb-alerts/dashboard')
         });
     });
 
-// catch and forward 404
-app.use((req, res, next) => {
-    let err = new Error('404 - Not Found');
-    err.status = 404;
-    next(err);
-});
-
-app.listen(PORT);
-console.log('app listening on port: ' + PORT);
+module.exports = router;
