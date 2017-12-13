fcfc6813301ddd09bceae5ff8175ba3088c9eb5b
Stephen Roman <stephenjroman@protonmail.com>
2017-12-12 19:44:58 -0500

    Add periodic email updates
    - refactored SendMail functions to be asynchronous

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const WebServer = require('./WebServer.js');
 const Calcs = require('./libs/Calcs');
+const SendMail = require('./libs/SendMail');
+const fs = require('fs');
+const path = require('path');
+const async = require('async');
 
+let currentPrice = 0;
 let mayerIndex = 0;
 let twoHundredDayMovingAverage = 0;
 let lastUpdated = 0;
@@ -10,11 +15,31 @@ run = function () {
     WebServer.start();
     console.log('Server started');
 
+    lastUpdated = new Date();
+
+    Calcs.getCurrentPrice('BTC-USD', (price) => {
+        currentPrice = price;
+    });
+
+    Calcs.get200DayMovingAverage('BTC-USD', (tdma) => {
+        twoHundredDayMovingAverage = tdma;
+    });
+
+    Calcs.getMayerIndex((mi) => {
+        mayerIndex = mi;
+    });
+
+    // dashboard page update
     setInterval(() => {
 
         lastUpdated = new Date();
+
+        Calcs.getCurrentPrice('BTC-USD', (price) => {
+            currentPrice = price;
+        });
+
         Calcs.get200DayMovingAverage('BTC-USD', (tdma) => {
-            twoHundredDayMovingAverage = tdma;     // not updating
+            twoHundredDayMovingAverage = tdma;
         });
 
         Calcs.getMayerIndex((mi) => {
@@ -22,6 +47,26 @@ run = function () {
         })
 
     }, 60000 * 10); // ten minutes
+
+    // email updates
+    setInterval(() => {
+        fs.readFile(path.join(__dirname, 'emails', 'users.txt'), (err, usersData) => {
+            if (err) throw err;
+                async.each(usersData.toString().split('\n'), (user, next) => {
+                    if (user)   // don't do blank lines
+                        SendMail.send('cbalerts@sroman.info', 'cbalerts Price Update', user,
+                            'This is an automated message, please do not respond.' + '\n' + '\n' +
+                            'Bitcoin\n' + '\n' + '\n' +
+                            'Current Price: ' + currentPrice + '\n' +
+                            '200 Daily Moving Average: ' + twoHundredDayMovingAverage + '\n' +
+                            'Mayer Index: ' + mayerIndex + '\n' + '\n' +
+                            'cbalerts', () => {
+                                console.log('Mail to ' + user + ' sent.');
+                            });
+                    next();
+            })
+        })
+    }, 60000 * 60 * 12); // twelve hours
 };
 
 exports.getMayerIndex = function() {
--- src/libs/SendMail.js
+++ src/libs/SendMail.js
@@ -1,66 +1,87 @@
 const child_process = require('child_process');
 const fs = require('fs');
 const path = require('path');
+const async = require('async');
 
-class SendMail {
+/**
+ * Use the Ubuntu Sendmail utility to send an email to someone
+ * This also saves a copy of the sent email to /src/emails/sent
+ * TODO add logging to sendmail to be sure mail is actually being sent
+ *
+ * @param sender: string:       email address (or name) of sender
+ * @param subject: string:      subject line
+ * @param recipients: string:   email addresses of recipients separated by a single space
+ * @param message: string:      body of the message to send
+ * @param callback
+ */
+exports.send = function (sender, subject, recipients, message, callback) {
 
-    /**
-     * Use the Ubuntu Sendmail utility to send an email to someone
-     * This also saves a copy of the sent email to /src/emails/sent
-     *
-     * @param sender: string:       email address (or name) of sender
-     * @param subject: string:      subject line
-     * @param recipients: string:   email addresses of recipients separated by a single space
-     * @param message: string:      body of the message to send
-     * @returns {boolean}           true if it didn't fail lol
-     */
-    static send(sender, subject, recipients, message) {
+    // construct email content
+    let content =
+        'To: ' + recipients + '\n' +
+        'Subject: ' + subject + '\n' +
+        'From: ' + sender + '\n' +
+        '\n' +
+        message;
 
-        // construct email content
-        let content =
-            'To: ' + recipients + '\n' +
-            'Subject: ' + subject + '\n' +
-            'From: ' + sender + '\n' +
-            '\n' +
-            message;
+    let filePath = path.join(__dirname, '..', 'emails', 'sent' , '/');
+    let fileName = 'message_' + new Date().toISOString() + '.txt';
 
-        let filePath = path.join(__dirname, '..', 'emails', 'sent' , '/');
-        let fileName = 'message_' + new Date().toISOString() + '.txt';
+    // save a copy
+    fs.writeFile(path.join(filePath, fileName), content, (err) => {
+        if (err) throw err;
+    });
 
-        // save a copy
-        fs.writeFileSync(path.join(filePath, fileName), content);
+    // set log level: -OLogLevel=9 NO WORKY
+    // specify log file: -D ./your/logfile.txt NO WORKY
 
-        // set log level: -OLogLevel=9 NO WORKY
-        // specify log file: -D ./your/logfile.txt NO WORKY
-        let command = 'sendmail -t < ' + filePath + this.findNewestFilename(filePath);
-
+    // use the recently saved copy of the message as the input for the cli command
+    let command = 'sendmail -t < ' + filePath;
+    findNewestFilename(filePath, (lastCreatedFile) => {
+        command = command + lastCreatedFile;
         console.log('Sending email to recipients: ' + recipients);
         console.log(command);
-        child_process.execSync(command);
-        console.log('Completed.');
+        child_process.exec(command, (err) => {
+            if (err) throw err;
+            callback();
+        });
+    });
+};
 
-        return true;
-    };
+/**
+ * Gets the filename of the most recently created file in a directory.
+ *
+ * @param dir: string:  directory to find the newest file in
+ * @param callback
+ * @returns string:     basename and extension of the newest file in dir
+ */
+function findNewestFilename(dir, callback) {
 
-    /**
-     * Gets the filename of the most recently created file in a directory.
-     *
-     * @param dir: string:  directory to find the newest file in
-     * @returns string:     basename and extension of the newest file in dir
-     */
-    static findNewestFilename(dir) {
+        let newest = '';    // filename to return
 
-        let newest = '';
-        let prev = 0;
+        let prevBD = 0;       // birthdate of previous file in loop
+
+        let remain = 0;     // number of files left to check
 
         // return basename + extension of newest file
-        fs.readdirSync(dir).forEach((file) => {
-            if (fs.fstatSync(1, dir + file).birthtime > prev) // 1 is file descriptor (read)
-                newest = file;
+        fs.readdir(dir, (err, files) => {
+            remain = files.length;
+            async.each(files, (file, next) => {
+                fs.stat(dir + file, (err, fileStats) => {
+                    if (err) throw err;
+                    if (fileStats.birthtime > prevBD)
+                        newest = file;
+                    prevBD = fileStats.birthtime;
+                    if (--remain === 0)
+                        callback(newest);
+                });
+                next();
+            }, (err) => {
+                if (err) throw err;
+            });
         });
-        return newest;
-    }
 }
 
-module.exports = SendMail;
-
+// findNewestFilename(path.join(__dirname, '..', 'emails', 'sent', '/'), (file) => {
+//     console.log(file);
+// });
